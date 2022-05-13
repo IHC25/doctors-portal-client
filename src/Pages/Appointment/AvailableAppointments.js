cc4b4230644ea37ef8e834970a8225de88a93e9e
arefin6 <imranhchy@gmail.com>
2022-05-13 22:38:28 +0600

    available appointment and booking modal added

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -1,8 +1,39 @@
 import { format } from "date-fns";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import BookingModal from "./BookingModal";
+import Service from "./Service";
 
 const AvailableAppointments = ({ date }) => {
-  return <div>Available Appointments on {format(date, "PP")}</div>;
+  const [services, setServices] = useState([]);
+  const [treatment, setTreatment] = useState(null);
+  useEffect(() => {
+    fetch("http://localhost:5000/service")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+  return (
+    <div>
+      <h4 className="text-xl text-center text-secondary">
+        Available Appointments on {format(date, "PP")}
+      </h4>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {services.map((service) => (
+          <Service
+            key={service._id}
+            service={service}
+            setTreatment={setTreatment}
+          ></Service>
+        ))}
+      </div>
+      {treatment && (
+        <BookingModal
+          treatment={treatment}
+          setTreatment={setTreatment}
+          date={date}
+        ></BookingModal>
+      )}
+    </div>
+  );
 };
 
 export default AvailableAppointments;
